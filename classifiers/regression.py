d241593dd7b475e6d9e416c3a866063439ba8cd1
Simon Giard-Leroux <sgiardleroux@gmail.com>
2020-12-01 17:54:17 -0500

    Implémentation de la classe principale (Classifier), Régression et SVM

---
+++ classifiers/regression.py
@@ -0,0 +1,8 @@
+from classifiers.classifier import Classifier
+from sklearn.linear_model import RidgeClassifier
+
+class Regression(Classifier):
+    def __init__(self, train_data, labels, test_data, test_ids, classes):
+        super(Regression, self).__init__(train_data, labels, test_data, test_ids, classes)
+        self.classifier = RidgeClassifier()
+        self.param_grid = {'alpha': [1e-1, 2e-1, 1e-2, 1e-3, 1e-4]}
